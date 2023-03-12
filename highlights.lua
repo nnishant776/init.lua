4bce2e321cf334cf0378c5f357b8c24196daadf1
nnishant776 <nn54565852@gmail.com>
2023-03-12 20:54:59 +0530

    add all highlights at startup

--- highlights.lua
+++ highlights.lua
@@ -25,6 +25,21 @@ return {
     }
   },
   add = {
+    CursorLine = {
+      bg = "black2",
+    },
+    DiffAdd = {
+      fg = "black",
+      bg = "green",
+    },
+    DiffChange = {
+      fg = "black",
+      bg = "sun"
+    },
+    DiffDelete = {
+      fg = "black",
+      bg = "red"
+    },
     DiffText = {
       fg = "black",
       bg = "blue"
