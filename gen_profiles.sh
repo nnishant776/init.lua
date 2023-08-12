713bc6f50dcd5be4f8ffd1a8dd5e9d1845b1179a
nnishant776 <nn54565852@gmail.com>
2023-08-12 11:47:12 +0530

    generate profiles individually

--- gen_profiles.sh
+++ gen_profiles.sh
@@ -4,18 +4,31 @@ ROOT="$HOME/.config/nvim/lua/custom"
 
 mkdir -p "$ROOT/profiles"
 
-rm -rf "$ROOT/../../plugin" "$HOME/.local/share/nvim"
-env features=minimal nvim
-mv "$ROOT/../../plugin/packer_compiled.lua" "$ROOT/profiles/packer_compiled_minimal.lua"
+rm -rf /tmp/nvim_temp
+cp -r "$HOME/.local/share/nvim" /tmp/nvim_temp
 
-rm -rf "$ROOT/../../plugin" "$HOME/.local/share/nvim"
-env features=default nvim
-mv "$ROOT/../../plugin/packer_compiled.lua" "$ROOT/profiles/packer_compiled_default.lua"
+case $1 in
+    "minimal" )
+    	rm -rf "$ROOT/../../plugin" "$HOME/.local/share/nvim"
+    	env features=minimal nvim
+    	mv "$ROOT/../../plugin/packer_compiled.lua" "$ROOT/profiles/packer_compiled_minimal.lua"
+        ;;
+    
+    "default" )
+        rm -rf "$ROOT/../../plugin" "$HOME/.local/share/nvim"
+        env features=default nvim
+        mv "$ROOT/../../plugin/packer_compiled.lua" "$ROOT/profiles/packer_compiled_default.lua"
+        ;;
 
-rm -rf "$ROOT/../../plugin" "$HOME/.local/share/nvim"
-env env features=ide,extconfig nvim
-mv "$ROOT/../../plugin/packer_compiled.lua" "$ROOT/profiles/packer_compiled_ide.lua"
+    "ide" )
+        rm -rf "$ROOT/../../plugin" "$HOME/.local/share/nvim"
+        env env features=ide,extconfig nvim
+        mv "$ROOT/../../plugin/packer_compiled.lua" "$ROOT/profiles/packer_compiled_ide.lua"
+        ;;
 
-rm -rf "$ROOT/../../plugin" "$HOME/.local/share/nvim"
-env features=all nvim
-mv "$ROOT/../../plugin/packer_compiled.lua" "$ROOT/profiles/packer_compiled_all.lua"
+    "all" )
+        rm -rf "$ROOT/../../plugin" "$HOME/.local/share/nvim"
+        env features=all nvim
+        mv "$ROOT/../../plugin/packer_compiled.lua" "$ROOT/profiles/packer_compiled_all.lua"
+        ;;
+esac
