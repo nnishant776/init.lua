0669fae0c0542936950a9174db3e1f67d31a4f97
nnishant776 <nn54565852@gmail.com>
2023-08-12 14:56:56 +0530

    update ui override for minimal and default mode

--- gen_profiles.sh
+++ gen_profiles.sh
@@ -9,9 +9,9 @@ cp -r "$HOME/.local/share/nvim" /tmp/nvim_temp
 
 case $1 in
     "minimal" )
-    	rm -rf "$ROOT/../../plugin" "$HOME/.local/share/nvim"
-    	env features=minimal nvim
-    	mv "$ROOT/../../plugin/packer_compiled.lua" "$ROOT/profiles/packer_compiled_minimal.lua"
+        rm -rf "$ROOT/../../plugin" "$HOME/.local/share/nvim"
+        env features=minimal nvim
+        mv "$ROOT/../../plugin/packer_compiled.lua" "$ROOT/profiles/packer_compiled_minimal.lua"
         ;;
     
     "default" )
