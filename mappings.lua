b1c426a42098ddedb87d31821dda7dc241cb3bb0
nnishant776 <nn54565852@gmail.com>
2023-09-03 02:31:00 +0530

    add keymap to jump based on treesitter context

--- mappings.lua
+++ mappings.lua
@@ -4,6 +4,45 @@ local function termcodes(str)
   return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
+local function get_context_range()
+  local start_line = -1
+  local start_col = -1
+  local end_line = -1
+  local end_col = -1
+
+  local ts_utils_status, ts_utils = pcall(require, "nvim-treesitter.ts_utils")
+  if not ts_utils_status then
+    return start_line, start_col, end_line, end_col
+  end
+
+  local locals = require "nvim-treesitter.locals"
+  local cursor_node = ts_utils.get_node_at_cursor()
+
+  local current_scope = locals.containing_scope(cursor_node, 0)
+  if not current_scope then
+    return start_line, start_col, end_line, end_col
+  end
+
+  while cursor_node do
+    local node_start_line, node_start_col, node_end_line, node_end_col = cursor_node:range()
+    local curr_line = vim.fn.line('.')
+    if curr_line > 0 then
+      curr_line = curr_line - 1
+    end
+    if node_start_line ~= node_end_line and node_start_line ~= curr_line and node_end_line ~= curr_line then
+      start_line = node_start_line
+      start_col = node_start_col
+      end_line = node_end_line
+      end_col = node_end_col
+      return start_line, start_col, end_line, end_col
+    end
+    cursor_node = cursor_node:parent()
+  end
+
+  vim.cmd [[normal! _]]
+  return start_line, start_col, end_line, end_col
+end
+
 local M = {}
 
 M.disabled = {
@@ -67,6 +106,37 @@ M.general = {
     ["k"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', opts = { expr = true } },
     ["<Up>"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', opts = { expr = true } },
     ["<Down>"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', opts = { expr = true } },
+    ["[{"] = {
+      function()
+        local start_line, start_col, _, _ = get_context_range()
+        if start_line == -1 then
+          vim.cmd [[ normal! [{ ]]
+          return
+        end
+        start_line = start_line + 1
+        vim.api.nvim_win_set_cursor(0, { start_line, start_col })
+        vim.cmd [[normal! _]]
+      end,
+      "Jump to context start",
+    },
+    ["]}"] = {
+      function()
+        local _, _, end_line, end_col = get_context_range()
+        if end_line == -1 then
+          vim.cmd [[ normal! ]} ]]
+          return
+        end
+        local last_line = vim.fn.line('$')
+        if end_line >= last_line then
+          end_line = last_line
+        else
+          end_line = end_line + 1
+        end
+        vim.api.nvim_win_set_cursor(0, { end_line, end_col })
+        vim.cmd [[normal! _]]
+      end,
+      "Jump to context end",
+    },
 
     -- new buffer
     ["<leader>bE"] = { "<cmd> enew <CR>", "New buffer" },
