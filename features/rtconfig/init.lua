cd23151ddd1062402e0d7fdb7abe252559a6ea0c
nnishant776 <nn54565852@gmail.com>
2023-09-16 20:31:34 +0530

    only load workspace config when external settings parsing is enabled

--- features/rtconfig/init.lua
+++ features/rtconfig/init.lua
@@ -54,9 +54,8 @@ local default_cfg = {
   }
 }
 
-local vsc_cfg_parser = require("custom.features.rtconfig.vscode")
-
 if vim.g.parse_external_editor_config then
+  local vsc_cfg_parser = require("custom.features.rtconfig.vscode")
   return vsc_cfg_parser.parse_config(default_cfg)
 else
   return default_cfg
