804ee12a598acb76bd94acf766eff7b3c2ce618e
nnishant776 <nn54565852@gmail.com>
2023-09-16 23:19:06 +0530

    update default language server install list

--- plugins/overrides/mason.lua
+++ plugins/overrides/mason.lua
@@ -2,10 +2,5 @@ return {
   ensure_installed = {
     "lua-language-server",
     "luau-lsp",
-    "bash-language-server",
-    "yaml-language-server",
-    "pyright",
-    "json-lsp",
-    "dockerfile-language-server",
   },
 }
