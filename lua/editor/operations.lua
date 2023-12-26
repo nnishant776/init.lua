5695766967e62ca9a67abad10746e6066f3a0f6a
nnishant776 <nn54565852@gmail.com>
2023-12-26 23:16:59 +0530

    fix trimming trailing whitespace logic

--- lua/editor/operations.lua
+++ lua/editor/operations.lua
@@ -19,9 +19,9 @@ function M.trim_trailing_whitespace(buf_id)
   for line = total_lines - 1, -1, -1 do
     local lines = vim.api.nvim_buf_get_lines(buf_id, line, line + 1, true)
     local line_content = lines[1]
-    if line_content ~= '' then
-      vim.api.nvim_buf_set_lines(buf_id, line, line + 1, true, { vim.trim(line_content) })
-      break
+    if line_content ~= nil and line_content ~= '' then
+      line_content, _ = string.gsub(line_content, '\\s$', '')
+      vim.api.nvim_buf_set_lines(buf_id, line, line + 1, true, { line_content })
     end
   end
 end
