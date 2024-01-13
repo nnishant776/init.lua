27cb33a550e0089f35304fda4300e79a6fb1921c
nnishant776 <nn54565852@gmail.com>
2024-01-13 14:40:37 +0530

    cosmetic changes

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -47,14 +47,14 @@ function M.setup(profile, editorconfig)
     opts = vim.tbl_deep_extend("force", opts, {
       enabled = function()
         -- disable completion in comments
-        local context = require "cmp.config.context"
+        local context = require("cmp.config.context")
         -- keep command mode completion enabled when cursor is in a comment
         if vim.api.nvim_get_mode().mode == "c" then
           return true
         else
           local is_suggest_enabled = editorconfig.editor.suggest.enabled
-          local is_comment_suggest_enabled = not context.in_treesitter_capture "comment"
-              and not context.in_syntax_group "Comment"
+          local is_comment_suggest_enabled = not context.in_treesitter_capture("comment")
+              and not context.in_syntax_group("Comment")
           if not is_suggest_enabled then
             return false
           end
